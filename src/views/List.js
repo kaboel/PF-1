d0af38896a98d734d6d5ca620474a367c125e4ed
Faiq <faiq.kaboel@gmail.com>
2020-02-07 05:16:14 +0700

    configure list page

---
+++ src/views/List.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+  render() {
+    return (
+      <div>
+        yolo
+      </div>
+    );
+  }
+}
+
+export default List;
