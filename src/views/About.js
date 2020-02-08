b89f64ee98092c1f2a36de94b7af996341fd5211
Faiq <faiq.kaboel@gmail.com>
2020-02-09 02:55:24 +0700

    setting up data and display them to each pages

--- src/views/About.js
+++ src/views/About.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
+import user from '../data/user';
+import faiq from '../assets/images/4x4.png'
 
 class About extends Component {
   render() {
     return (
-      <div>
-        Yolo
+      <div className="container">
+        <div class="row justify-content-center mt-5 mb-4">
+          <h4>ABOUT</h4>
+        </div>
+        <div className="row justify-content-center mb-3">
+          <div className="col-md-2 text-center">
+            <img className="img-fluid img-thumbnail rounded mx-auto d-block" src={faiq} alt={faiq}/>
+          </div>
+          <div className="col-md-4">
+            <div className="row">
+              <div className="col">
+                <h2><u>{user.name}</u></h2>
+                <span>{user.nim}</span>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col mt-3">
+                <h5>{user.class}</h5>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <h5>{user.dob}</h5>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <h5>{user.email}</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <hr/>
+
+          </div>
+        </div>
       </div>
     );
   }
--- src/views/List.js
+++ src/views/List.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import mhs from '../data/mhs'
 
 class List extends Component {
   render() {
     return (
-      <div>
-        yolo
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-4 text-center mt-5 mb-5">
+            <h4>MHS LIST</h4>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-md-10 col-lg-10">
+            <table className="table table-striped table-dark">
+              <thead>
+              <tr>
+                <th>NAME</th>
+                <th>NIM</th>
+                <th>DATE OF BIRTH</th>
+                <th>PHONE</th>
+              </tr>
+              </thead>
+              <tbody>
+              {mhs.map(el => {
+                return <tr>
+                  <td>{el.name}</td>
+                  <td>{el.nim}</td>
+                  <td>{el.dob}</td>
+                  <td>{el.phone}</td>
+                </tr>
+              })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
